a8c1bdd6de44d8ff71b3500c1292fca32af5ee80
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-19 21:40:21 +0700

    set operation

--- src/test/java/programmerzamannow/redis/RedisTest.java
+++ src/test/java/programmerzamannow/redis/RedisTest.java
@@ -4,12 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 import java.time.Duration;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class RedisTest {
@@ -45,4 +50,21 @@ public class RedisTest {
     assertEquals("Kurniawan", operations.leftPop("names"));
     assertEquals("Khannedy", operations.leftPop("names"));
   }
+
+  @Test
+  void set() {
+    SetOperations<String, String> operations = redisTemplate.opsForSet();
+
+    operations.add("students", "Eko");
+    operations.add("students", "Eko");
+    operations.add("students", "Kurniawan");
+    operations.add("students", "Kurniawan");
+    operations.add("students", "Khannedy");
+    operations.add("students", "Khannedy");
+
+    Set<String> students = operations.members("students");
+    assertEquals(3, students.size());
+    assertThat(students, hasItems("Eko", "Kurniawan", "Khannedy"));
+  }
 }
+
