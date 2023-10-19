616ecb4661827423646f501e57c543b68ffffefe
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-19 10:38:08 +0700

    create project

---
+++ src/main/java/programmerzamannow/redis/BelajarSpringRedisApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/redis/BelajarSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
