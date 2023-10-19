8739313d6c46739ed1ceb5e877a36a87120f53e8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-19 21:31:12 +0700

    value operation

--- src/test/java/programmerzamannow/redis/StringTest.java
+++ src/test/java/programmerzamannow/redis/StringTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -19,5 +22,16 @@ public class StringTest {
   @Test
   void redisTemplate() {
     assertNotNull(redisTemplate);
+  }
+
+  @Test
+  void string() throws InterruptedException {
+    ValueOperations<String, String> operations = redisTemplate.opsForValue();
+
+    operations.set("name", "Eko", Duration.ofSeconds(2));
+    assertEquals("Eko", operations.get("name"));
+
+    Thread.sleep(Duration.ofSeconds(3));
+    assertNull(operations.get("name"));
   }
 }
